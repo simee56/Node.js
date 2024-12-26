cdc594b0c759af8b650eff1e96ed099b9d5ac073
simee56 <pandeysimee7@gmail.com>
2024-12-26 23:45:28 +0530

    os modules

--- os.js
+++ os.js
@@ -1,3 +1,15 @@
 const os = require('os');
 
 let totalMemory = os.totalmem();
+let freeMemory = os.freemem();
+let upTime = os.uptime();
+let hostName = os.hostname();
+let type = os.type();
+
+console.log(totalMemory);
+console.log(freeMemory);
+console.log(upTime);
+console.log(hostName);
+console.log(type);
+
+
