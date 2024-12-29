f7610429f80376a95997f4bf8e229d498c562bfe
simee56 <pandeysimee7@gmail.com>
2024-12-29 14:13:52 +0530

    updtae file.js

--- file.js
+++ file.js
@@ -1,7 +1,9 @@
+//creating a file
+
 const fs = require("fs");
 
 //sync file
 fs.writeFileSync("./hey.txt", "this is John");
 
 //async file 
-fs.writeFile("./hey.txt", "this is John async",(err) => {});
+// fs.writeFile("./hey.txt", "this is John async",(err) => {});
