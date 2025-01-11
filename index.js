f0dc89755ac894aa11c7f0094daf88a2b7a27676
Simee Pandey <164655969+simee56@users.noreply.github.com>
2025-01-11 22:49:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+// Importing the function which is used in math.json with the help of require function.
+const math = require('./math.js');
+console.log("The value of math is : ", math.add(4, 5));
+
+//OR
+
+const { add, sub } = require('./math.js');
+console.log("The value of math is : ", sub(4, 5));
+
+// console.log("The value of math is : ",math);
+
+
