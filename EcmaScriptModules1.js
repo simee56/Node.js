8b53b38864355c9e37cfaf2c996d0e79aaa941ee
Simee Pandey <164655969+simee56@users.noreply.github.com>
2025-01-13 14:26:53 +0530

    Create EcmaScriptModules1.js

---
+++ EcmaScriptModules1.js
@@ -0,0 +1,15 @@
+// ECMAScript modules
+
+export function info() {
+    console.log("Here we are learning ECMAScript modules ")
+}
+
+export function info2() {
+    console.log("Importing info2 in our ECMAScript2")
+};
+
+
+//if we don't want to use the { } to import the function then this function will be executed.
+export default function Default() {
+    console.log("Default function")
+};
