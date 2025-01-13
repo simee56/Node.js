c83722d4871b8b78bc6b47d8b63810a3fec4e2f7
Simee Pandey <164655969+simee56@users.noreply.github.com>
2025-01-13 14:38:41 +0530

    Update fileAsync.js

--- fileAsync.js
+++ fileAsync.js
@@ -1,40 +1,26 @@
 const fs = require("fs");
 
 
-// //creating a file
-// fs.writeFile("./hey.txt", "this is John.",(err) => {});     
+//creating a file
+fs.writeFile("./hey.txt", "this is John.",(err) => {});     
 
 
-// // reading a file
+// reading a file
 
-// fs.readFile("./info.txt", "utf-8", (err, result) =>{            
-//     if(err){
-//         console.log("Error : ", err);
-//     }
-//     else{
-//         console.log("Result : ", result);
-//     }
-// })
-
-
-// //append function
-
-// fs.appendFile("./info.txt", "+9444444", (err) => {
-//     if(err){
-//         console.log("Error : ", err);
-//     }
-//     else{
-//         console.log("Result : ");
-//     }
-// })
-
-
-fs.chmod("./info.txt", 0o75, (err) => {
-    if (err) {
-        console.log("Error :", err)
+fs.readFile("./info.txt", "utf-8", (err, result) =>{            
+    if(err){
+        console.log("Error : ", err);
     }
     else{
-    console.log('The permissions for file "my_file.txt" have been changed!')
+        console.log("Result : ", result);
     }
-}); 
+})
 
+
+//append function
+
+fs.appendFile("./info.txt", "\nand enjoy this journey", (err) => {
+    if(err){
+        console.log("Error : ", err);
+    };
+});
